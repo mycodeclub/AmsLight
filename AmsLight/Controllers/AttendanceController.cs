407203ea67bef73ccae9b0b87d3aba9259139928
mycodeclub <ankitsahay1990@gmail.com>
2019-03-22 13:34:42 +0530

    Updated Attendance - In progress...

--- AmsLight/Controllers/AttendanceController.cs
+++ AmsLight/Controllers/AttendanceController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AmsLight.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +7,21 @@ using System.Web.Mvc;
 
 namespace AmsLight.Controllers
 {
+    [Authorize]
     public class AttendanceController : Controller
     {
+        private AmsDbContext db = new AmsDbContext();
+
         // GET: Attendances
-        public ActionResult Index()
+        public ActionResult Index(int tcId = 2, int batchId = 1)
         {
+            var tpId = Convert.ToInt32(System.Web.HttpContext.Current.User.Identity.Name);
+            ViewBag.TrainingCenters = db.TrainingCenters.Where(tc => tc.TpId == tpId).ToList();
+            ViewBag.Batches = db.Batches.Where(b => b.TrainingCenterId == tcId).ToList();
+            ViewBag.Students = db.Students.Where(s => s.BatchId == batchId).ToList();
+            ViewBag.SelectedTcId = tcId;
+            ViewBag.SelectedBatchId = batchId;
+
             return View();
         }
     }
