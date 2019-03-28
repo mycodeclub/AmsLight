d02cb1f85555f8aba65b1a1cf046701360e81529
mycodeclub <ankitsahay1990@gmail.com>
2019-03-28 16:49:43 +0530

    Added Encode Decode methods for Ids.
    Use Following
    	 var encodedId = Common.Secure.Encode(id.Value);
                 var Decode = Common.Secure.Decode(encodedId);

---
+++ AmsLight/Common/Secure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmsLight.Common
+{
+    public static class Secure
+    {
+        public static string Encode(string encodeMe)
+        {
+            byte[] encoded = System.Text.Encoding.UTF8.GetBytes(encodeMe);
+            return Convert.ToBase64String(encoded);
+        }
+        public static string Encode(int id)
+        {
+            byte[] encoded = System.Text.Encoding.UTF8.GetBytes(Convert.ToString(id));
+            return Convert.ToBase64String(encoded);
+        }
+
+        public static int Decode(string decodeMe)
+        {
+            byte[] encoded = Convert.FromBase64String(decodeMe);
+            return Convert.ToInt32(System.Text.Encoding.UTF8.GetString(encoded));
+        }
+    }
+}
