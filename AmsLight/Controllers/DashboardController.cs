969e02e3ced06d4888cdf02387e8fad92776b579
mycodeclub <ankitsahay1990@gmail.com>
2019-08-24 18:23:19 +0530

    Linked with BPST

--- AmsLight/Controllers/DashboardController.cs
+++ AmsLight/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AmsLight.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,22 @@ namespace AmsLight.Controllers
     [Authorize]
     public class DashboardController : Controller
     {
+        private AmsDbContext db = new AmsDbContext();
         // GET: Dashboard
         public ActionResult Index()
         {
+            int tpId = Convert.ToInt32(System.Web.HttpContext.Current.User.Identity.Name);
+            ViewData["BatchCount"] = (from tc in db.TrainingCenters
+                                      join b in db.Batches on tc.TrainingCenterId equals b.TrainingCenterId
+                                      where tc.TpId == tpId
+                                      select new { b.BatchId }).Count();
+
+            var tcCount= (from tc in db.TrainingCenters where tc.TpId == tpId select tc).Count();
+            ViewData["TrainingCentersCount"] = tcCount;
+             //join b in db.Batches on tc.TrainingCenterId equals b.TrainingCenterId
+
+
+
             return View();
         }
     }
